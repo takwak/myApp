ad7249d667d4da2579909b9e0b4614aa343833e5
takwa_ <takwakhalil7@gmail.com>
2025-07-05 07:19:07 +0200

    commit

--- Home/urls.py
+++ Home/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.app_view, name='home'),  
-    path('SmartPath/', views.app_view, name='app'),  
-    path('login/', views.login_view, name='login'),  
-    path('signup/', views.sign_view, name='signup'),  
-    path('home/', views.homepage, name='dashboard'),  
-    path('my-academic-path/', views.academic_path, name='academic_path'),  
-    path('logout/', views.logout_view, name='logout'),  
+    path('', views.app_view, name='App'),
+    path('SmartPath/', views.app_view, name='App'),
+    path('LogIn/', views.login_view, name='LogIn'),
+    path('SignUp/', views.sign_view, name='SignUp'),
+    path('Home/', views.homepage, name='Home'),
+    path('MyAcademicPath/', views.academic_path, name='MyAcademicPath'),
+    path('logout/', views.logout_view, name='LogOut'),
     path('save-goal/', views.save_goal, name='save_goal'),
-    path('save-department/', views.save_department, name='save_department'),
+    path('save-department/', views.save_department, name='save_department'),  
     path('curriculum-map/<int:department_id>/', views.curriculum_map, name='curriculum_map'),
-    path('profile/', views.profile_view, name='profile'),  
+    path('Profile/', views.profile_view, name='Profile'),
     path('save-plan/', views.save_plan, name='save_plan'),
     path('delete-plan/', views.delete_plan, name='delete_plan'),
+ 
 ]
